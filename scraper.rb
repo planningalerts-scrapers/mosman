ec9b9026f53aee53836394f0dc05dfd506f5e105
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 02:29:10 +1000

    Upgrade to latest gem

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,3 @@
 require "icon_scraper"
 
-IconScraper.scrape_with_params(
-  url: "http://portal.mosman.nsw.gov.au",
-  period: "last14days",
-  types: [8, 5]
-) do |record|
-  IconScraper.save(record)
-end
+IconScraper.scrape_and_save(:mosman)
