8a0cd94e19b845295cdcbfa7f7f7c8464165beed
Matthew Landauer <mlandauer@gmail.com>
2019-06-10 02:26:10 +1000

    Go back to original form of info_url and address

--- scraper.rb
+++ scraper.rb
@@ -5,7 +5,5 @@ IconScraper.scrape_with_params(
   period: "last14days",
   types: [8, 5]
 ) do |record|
-  record["info_url"] = record["info_url"].gsub("/Pages/XC.Track/", "/pages/xc.track/")
-  record["address"] = record["address"].gsub("MOSMAN NSW 2088", "Mosman, NSW")
   IconScraper.save(record)
 end
